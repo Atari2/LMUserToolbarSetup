f95c4d7fe3360402c2db2e7350d98e84638eefad
Atari2 <alex.robassomero@gmail.com>
2021-03-09 20:41:36 +0100

    Add project files.

---
+++ UserToolbarSetup/UserToolbarSetup.cpp
@@ -0,0 +1,76 @@
+#include "UserToolbarSetup.h"
+#include <algorithm>
+#include <cstdio>
+#include <filesystem>
+#include <string>
+
+namespace fs = std::filesystem;
+
+int main(int argc, char *argv[]) {
+
+    char cpath[_MAX_PATH] = {0};
+    char startstring[] = "***START***\nLM_SPACER\n***END***\n***START***\n\"";
+    char endstring[] = "\" -lm-handle \"%9\" \"%1\"\n0, Runs Pixi on the rom, reloads it after.\n***END**";
+    constexpr int tot_len = 45 + _MAX_PATH + 74 + 1; // strlen(startstring) + MAX_PATH + strlen(endstring) + \0
+    char totalstring[tot_len] = {0};
+    printf("Drag and drop Lunar Magic's executable here:\n");
+    gets_s(cpath, (unsigned)_countof(cpath));
+    if (cpath[0] == '"' && cpath[strlen(cpath) - 1] == '"') {
+        cpath[strlen(cpath) - 1] = '\0';
+        memmove(cpath, cpath + 1, strlen(cpath) + 1);
+    }
+    fs::path lmpath(cpath);
+    if (lmpath.is_relative() || lmpath.extension() == "") {
+        Error("Path wasn't correct. Path was:", lmpath);
+    }
+
+    std::string strPath = lmpath.parent_path().generic_string() + "/usertoolbar.txt";
+
+    FILE *out = nullptr;
+    auto err = fopen_s(&out, strPath.c_str(), "w");
+
+    if (err != 0 || out == nullptr) {
+        Error("Couldn\'t open file ", strPath.c_str());
+    }
+
+    std::vector<fs::directory_entry> dires{};
+    for (const auto &de : fs::directory_iterator(fs::current_path())) {
+        if (de.is_regular_file() && de.exists()) {
+            dires.emplace_back(de);
+        }
+    }
+
+    auto res = std::find_if(dires.begin(), dires.end(), [](const fs::directory_entry &de) {
+        const auto &p = de.path();
+        return p.filename() == "pixi.exe";
+    });
+
+    if (res == dires.end()) {
+        Error("Couldn't find pixi's executable in the current folder");
+    }
+
+    fs::path pixiPath = *res;
+
+    std::string strpixiPath = fs::absolute(pixiPath).generic_string();
+
+    char cstrpixiPath[_MAX_PATH];
+    err = strcpy_s(cstrpixiPath, _MAX_PATH, strpixiPath.c_str());
+
+    if (err != 0) {
+        Error("Error while copying path");
+    }
+
+    int ret = sprintf_s(totalstring, "%s%s%s", startstring, cstrpixiPath, endstring);
+
+    totalstring[tot_len - 1] = '\0';
+
+#pragma warning(disable : 6387)
+
+    fprintf(out, "%s", totalstring);
+
+    fclose(out);
+
+    printf("usertoolbar.txt created successfully in your Lunar Magic folder!");
+    ret = getchar();
+    return 0;
+}
---
+++ UserToolbarSetup/UserToolbarSetup.h
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+
+template <typename T, auto P> class AddPre {
+  private:
+    const T &ref;
+
+  public:
+    AddPre(const T &r) : ref(r) {}
+    friend std::ostream &operator<<(std::ostream &os, AddPre<T, P> s) { return os << P << s.ref; }
+};
+
+template <typename First, typename... Args> void Error(First firstarg, Args... args) {
+    std::cout << firstarg;
+    (std::cout << ... << AddPre<Args, ' '>(args)) << '\n';
+    int a = getchar();
+    exit(0);
+}
